39f452c47099474fbb3ed2d09dc0c7e90ef2fefc
sumaiya-rahman-chowdhury <sumaiyaprionty@gmail.com>
2022-10-02 14:58:14 +0600

    First Commit

---
+++ obj.js
@@ -0,0 +1,6 @@
+const person = {
+	firstName:"John" , lastName:"Smith" ,
+	age:50 , eyeColor:"blue"
+};
+document.getElementById("p01").innerHTML = 
+person.firstName + " is " + person.age + " old";
