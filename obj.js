5bc4f2e1c31ac5cc4be5941eeb9fb7bb5f3ec882
Sumaiya-rahman-chowdhury <sumaiyaprionty@gmail.com>
2022-10-05 08:36:15 -0700

    4 commit

--- obj.js
+++ obj.js
@@ -1,6 +1,18 @@
 const person = {
-	firstName:"John" , lastName:"Smith" ,
+	firstName:"John" , 
+	lastName:"Smith" ,
 	age:50 , eyeColor:"blue"
 };
 document.getElementById("p01").innerHTML = 
 person.firstName + " is " + person.age + " old";
+const person2 = {
+	firstName2:"John",
+	lastName2:"Doe",
+	age2:" ",
+	set agee(value)
+	{
+		this.age2 = value;
+	}
+};
+person2.age2 = 55;
+document.getElementById("p03").innerHTML=person2.age2;
